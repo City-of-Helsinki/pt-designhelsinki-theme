da9a34c90a2d5b42e116bd94b95c73133c8f793e
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-12-03 11:54:33 +0200

    Update header.php

--- header.php
+++ header.php
@@ -19,28 +19,9 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 		<?php wp_head(); ?>
-
-		<!-- Matomo -->
-		<script type="text/javascript">
-			var _paq = window._paq = window._paq || [];
-			/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-			_paq.push(['trackPageView']);
-			_paq.push(['enableLinkTracking']);
-			(function() {
-				var u="//webanalytics.digiaiiris.com/js/";
-				_paq.push(['setTrackerUrl', u+'tracker.php']);
-				_paq.push(['setSiteId', '244']);
-				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-				g.type='text/javascript'; g.async=true; g.src=u+'piwik.min.js'; s.parentNode.insertBefore(g,s);
-			})();
-		</script>
-		<!-- End Matomo Code -->
-
 	</head>
 
 	<body <?php body_class(); ?>>
-
-
 
 		<div id="page" class="site">
 
