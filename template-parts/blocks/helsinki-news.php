0e608a8742e9cc7a1eeb35af8ab66103af401e5c
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-10 11:48:22 +0300

    Update helsinki-news.php

--- template-parts/blocks/helsinki-news.php
+++ template-parts/blocks/helsinki-news.php
@@ -1,4 +1,4 @@
-  <?php
+<?php
 
   /**
   * Helsinki API News Block
@@ -14,42 +14,42 @@
     'category_name' => 'palvelumuotoilu-ja-design',
   );
 
-  if ($cat) {
-   $args['cat'] = $cat;
- }
+if (! empty($cat)) {
+	$args['cat'] = $cat;
+}
 
 
- $query = new WP_Query($args);
+$query = new WP_Query($args);
 
- if ($query->have_posts()) :
-  global $post;
-  ?> 
-  <section id="uutiset" class="section latest zoom">
-    <div class="container">
-      <h2 class="section-title"><?= $title ?></h2>
-      <div class="columns is-multiline">
-        <?php while ($query->have_posts()) : 
-          $query->the_post(); 
-          $ingressi = get_field('ingressi', $post->ID);
-          $image_id = get_post_thumbnail_id(get_the_ID());
-          $alt_text = get_post_meta($image_id , '_wp_attachment_image_alt', true);
-          ?>
+if ($query->have_posts()) :
+	global $post;
+	?>
+	<section id="uutiset" class="section latest zoom">
+		<div class="container">
+			<h2 class="section-title"><?= $title ?></h2>
+			<div class="columns is-multiline">
+				<?php while ($query->have_posts()) :
+					$query->the_post();
+					$ingressi = get_field('ingressi', get_the_ID());
+					$image_id = get_post_thumbnail_id(get_the_ID());
+					$alt_text = get_post_meta($image_id , '_wp_attachment_image_alt', true);
+					?>
 
-          <a href="<?php the_permalink(); ?>" class="column is-4 is-12-mobile">
-            <div class="element">
-              <figure class="image is-3by2">
-                <img class="is-square" src="<?= get_the_post_thumbnail_url( $post->ID, 'large' ); ?>" alt="<?= $alt_text; ?>">
-              </figure>
-              <div class="text-content">
-                <h3 class="title is-4 is-medium"><?php the_title(); ?></h3>
-                <p class="excerpt"><?= $ingressi; ?></p>
-              </div>
-              <div class="hds-icon hds-icon--size-l hds-icon--arrow-right"></div>
-            </div>
-          </a>
+					<a href="<?php the_permalink(); ?>" class="column is-4 is-12-mobile">
+						<div class="element">
+							<figure class="image is-3by2">
+								<img class="is-square" src="<?= get_the_post_thumbnail_url( get_the_ID(), 'large' ); ?>" alt="<?= $alt_text; ?>">
+							</figure>
+							<div class="text-content">
+								<h3 class="title is-4 is-medium"><?php the_title(); ?></h3>
+								<p class="excerpt"><?= $ingressi; ?></p>
+							</div>
+							<div class="hds-icon hds-icon--size-l hds-icon--arrow-right"></div>
+						</div>
+					</a>
 
-        <?php endwhile; wp_reset_postdata(); ?> 
-      </div>
-    </div>
-  </section>
-  <?php endif; ?>
+				<?php endwhile; wp_reset_postdata(); ?>
+			</div>
+		</div>
+	</section>
+<?php endif; ?>
