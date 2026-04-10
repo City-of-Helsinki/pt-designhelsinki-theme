91d414a1dffa0caad7cc17d6711780a3e2ffa7c0
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-10 11:49:42 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -11,56 +11,56 @@
 
 ?>
 
-</div><!-- #content -->
+			</div><!-- #content -->
 
-<footer class="footer wave-bottom bc-valkoinen">
-	<div class="container">
-		<div class="content">
-      <h2><?php pll_e('Yhteystiedot') ?></h2>
-      <a href="/yhteystiedot"><?php pll_e('Hae yhteystietoja') ?></a>
-    </div>
+			<footer class="footer wave-bottom bc-valkoinen">
+				<div class="container">
+					<div class="content">
+						<h2><?php pll_e('Yhteystiedot') ?></h2>
+						<a href="/yhteystiedot"><?php pll_e('Hae yhteystietoja') ?></a>
+					</div>
 
-    <?php
+					<div class="columns is-vcentered">
+						<div class="container">
+							<figure class="image">
+								<img src="<?= get_stylesheet_directory_uri() ?>/images/HELSINKI_Tunnus_VALKOINEN.png" alt="Footer logo">
+							</figure>
 
-    if( have_rows('footer_links', 'options') ): ?>
-      <div class="columns is-vcentered">
-        <div class="container">
-          <figure class="image">
-            <img src="<?= get_stylesheet_directory_uri() ?>/images/HELSINKI_Tunnus_VALKOINEN.png" alt="Footer logo">
-          </figure>
-          <div class="footer-info">
-            <span>&copy; <?php pll_e('Helsingin kaupunki') ?>, <?php echo date("Y"); ?></span> 
+							<?php if( have_rows('footer_links', 'options') ): ?>
+								<div class="footer-info">
+									<span>&copy; <?php pll_e('Helsingin kaupunki') ?>, <?php echo date("Y"); ?></span>
 
-            <?php
-            while( have_rows('footer_links', 'options') ) : the_row(); 
-              $title = get_sub_field('otsikko');
-              $title_en = get_sub_field('otsikko_en');
-              $title_sv = get_sub_field('otsikko_sv');
+									<?php while( have_rows('footer_links', 'options') ) :
+										the_row();
+										$title = get_sub_field('otsikko');
+										$title_en = get_sub_field('otsikko_en');
+										$title_sv = get_sub_field('otsikko_sv');
 
-              $link = get_sub_field('linkki');
-              $link_en = get_sub_field('linkki_en');
-              $link_sv = get_sub_field('linkki_sv');
+										$link = get_sub_field('linkki');
+										$link_en = get_sub_field('linkki_en');
+										$link_sv = get_sub_field('linkki_sv');
 
-              $lang = pll_current_language();
+										$lang = pll_current_language();
 
-              if ($lang == 'fi') {
-                echo '<a href="'.$link.'">'.$title.'</a>';
-              } elseif($lang == 'en') {
-                echo '<a href="'.$link_en.'">'.$title_en.'</a>';
-              } else {
-               echo '<a href="'.$link_sv.'">'.$title_sv.'</a>';
-             }
-             
-           endwhile;
-         endif; ?>
-       </div>
-     </div>
-   </div>
- </div>
-</footer>
-</div><!-- #page -->
+										if ($lang == 'fi') {
+											echo '<a href="'.$link.'">'.$title.'</a>';
+										} elseif($lang == 'en') {
+											echo '<a href="'.$link_en.'">'.$title_en.'</a>';
+										} else {
+											echo '<a href="'.$link_sv.'">'.$title_sv.'</a>';
+										}
 
-<?php wp_footer(); ?>
+									endwhile; ?>
+								</div>
+							<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			</footer>
 
-</body>
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
 </html>
